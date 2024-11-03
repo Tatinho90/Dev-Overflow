abd0ec6a0343a9387ee496fa162a1e996e9f7a20
Tatinho90 <101295790+Tatinho90@users.noreply.github.com>
2024-11-03 20:36:01 +0100

    Eslint and prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";0
+import Image from "next/image";
+0;
 
 export default function Home() {
   return (
-   <h1 className=" text-violet-700 text-3xl font-black">Welcome to the world of next.js</h1>
-
+    <>
+      <main>
+        <h1 className=" text-violet-700 text-3xl font-black">
+          Welcome to the world of next.js
+        </h1>
+        <h2 className="cool">Please make this look better</h2>
+        <h2>Hmmm will this work</h2>
+      </main>
+    </>
   );
 }
