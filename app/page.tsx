c3d4d4a6a2e2daf90386f30e0835d8b8fd01099b
Tatinho90 <101295790+Tatinho90@users.noreply.github.com>
2024-11-23 12:20:06 +0100

    TailwindCSS Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
-0;
 
 export default function Home() {
   return (
     <>
       <main>
-        <h1 className=" text-violet-700 text-3xl font-black">
+        <h1 className=" text-3xl font-black text-violet-700">
           Welcome to the world of next.js
         </h1>
-        <h2 className="cool">Please make this look better</h2>
+        <h2>Please make this look better</h2>
+        <h2>Hmmm will this work</h2>
+      </main>
+
+      <main>
+        <h1 className=" text-3xl font-black text-violet-700">
+          Welcome to the world of next.js
+        </h1>
+        <h2>Please make this look better</h2>
         <h2>Hmmm will this work</h2>
       </main>
     </>
