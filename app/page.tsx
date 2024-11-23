28e7abb86cc33a1518912235f95914fd768de091
Tatinho90 <101295790+Tatinho90@users.noreply.github.com>
2024-11-23 14:20:43 +0100

    TailWind Local Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
       <main>
-        <h1 className=" text-3xl font-black text-violet-700">
-          Welcome to the world of next.js
-        </h1>
+        <h1 className=" h1-bold">Welcome to the world of next.js</h1>
         <h2>Please make this look better</h2>
         <h2>Hmmm will this work</h2>
       </main>
 
       <main>
-        <h1 className=" text-3xl font-black text-violet-700">
-          Welcome to the world of next.js
+        <h1 className=" h1-bold font-space-grotesk text-primary-500">
+          Welcome to the world of next.js (INTER)
         </h1>
         <h2>Please make this look better</h2>
         <h2>Hmmm will this work</h2>
