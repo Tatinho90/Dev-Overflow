bb1bc61646ce13b65a4460e1591c399656bb2bc8
Tatinho90 <101295790+Tatinho90@users.noreply.github.com>
2024-11-27 21:14:27 +0100

    Installed ShadCn Ui + added a dropdown component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Home() {
   return (
     <>
@@ -5,6 +14,17 @@ export default function Home() {
         <h1 className=" h1-bold">Welcome to the world of next.js</h1>
         <h2>Please make this look better</h2>
         <h2>Hmmm will this work</h2>
+        <DropdownMenu>
+          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </main>
 
       <main>
